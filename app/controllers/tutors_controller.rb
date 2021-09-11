13de472c56b85164b9fd8dcc116e6f9170f091b4
dara <just4oludee@yahoo.com>
2021-09-10 21:55:46 -0400

    Tutor routes complete

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,8 +1,32 @@
 class TutorsController < ApplicationController
 
     def index
-        @tutors = Tutor.all
-        render json: @tutors
+        tutors = Tutor.all
+        render json: tutors, only: [:id, :name]
+    end
+
+    def show
+        id = params[:id]
+        tutor = Tutor.find(id)
+        render json: tutor
+    end 
+
+    def create
+        tutor = Tutor.create(tutor_params)
+        render json: tutor
+    end
+
+    def destroy
+        id = params[:id]
+        tutor = Tutor.find(id)
+        tutor.destroy
+        render json: {message: "Tutor destroyed successfully"}
+    end
+
+    private
+
+    def tutor_params
+        params.permit(:name)
     end
 
 end
